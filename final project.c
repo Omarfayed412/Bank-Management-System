6c7cb78941ba829b96f6a481e8b5d59eccef945e
YaseenIslam2005 <149319942+YaseenIslam2005@users.noreply.github.com>
2023-12-19 03:15:21 +0200

    Update on main structure

--- final project.c
+++ final project.c
@@ -2,24 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+typedef struct
 {
-	// file pointer variable to store the value returned by
-	// fopen
-	FILE* fptr;
-    char std[1000];
-	// opening the file in read mode
-	fptr = fopen("accounts.txt", "r");
+int mounth,year;
+}lastAccess;
+typedef struct
+{
+int account_number,mobile_number;
+char email_address[30];
+float balance;
+lastAccess dateOpened;
 
-	// checking if the file is opened successfully
-	if (fptr == NULL) {
-		printf("The file is not opened. The program will "
-			"now exit.");
-		exit(0);
-	}
-	int a;
-	fscanf(fptr,"%d",&a);
-	printf("%d",a);
-
-	return 0;
-}
+}Amin;
