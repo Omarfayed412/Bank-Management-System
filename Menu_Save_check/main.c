9571991ce2edaa178d5ac2f8624f327176df756a
Omar Fayed <omarfayed381@gmail.com>
2023-12-19 22:09:12 +0200

    Checking function and struct constructor function

--- Menu_Save_check/main.c
+++ Menu_Save_check/main.c
@@ -2,21 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+//unique username
 
 void Menu(void);
 char *StrToUpper(char *str);
-int checkAccountNo(char *accStr);
+void checkAccountNo(char *accStr);
+void SAVE();
+
+typedef struct
+{
+    int month,year;
+} Date;
+
+typedef struct
+{
+    char mobile_number[12];
+    char account_number[11];
+    char username[20];
+    char email_address[30];
+    double balance;
+    Date dateOpened;
+    FILE *fptr ;
+
+} Account;
+
+//Will be mainly used in ADD function
+Account *ConstructAccount(char *mobile, char *accNum, char *name, char *email, double balanceNum, Date *dateOpnd)
+{
+    //Reserving memory in the heap for new instance
+    Account *acc = malloc(sizeof(Account));
+    if (acc == NULL)
+    {
+        printf("No enough space!\n");
+        return NULL;
+    }
+    //Copying the arguments into the new instance arguments
+    strcpy(acc->account_number, accNum);
+    strcpy(acc->mobile_number, mobile);
+    strcpy(acc->email_address, email);
+    strcpy(acc->username, name);
+    acc->balance = balanceNum;
+    acc->dateOpened.month = dateOpnd->month;
+    acc->dateOpened.year = dateOpnd->year;
+
+    return acc;
+}
+
+
+//Will be used before closing the program
+void DestructAccount(Account *acc)
+{
+    free(acc);
+}
 
 int main()
 {
     char accnum[20];
-    gets(accnum);
+    Account array[50];
 
-    checkAccountNo(accnum);
+    //gets(accnum);
+    //checkAccountNo(accnum);
+    Menu();
     return 0;
 }
 
-void Menu(void){
+void Menu(void)
+{
     char choice[20];
     int flag;
 
@@ -26,7 +77,8 @@ void Menu(void){
 
 
 
-    do {
+    do
+    {
         flag = 1;
         printf("Desired option: ");
         gets(choice);
@@ -65,32 +117,29 @@ void Menu(void){
         else if (strcmp(choice, "QUIT") == 0)
             printf("QUIT\n");
 
-        else{
+        else
+        {
             printf("%s", "Invalid Option Entered!\n");
             flag = 0;
         }
-    } while (!flag);
+    }
+    while (!flag);
 }
 
-int checkAccountNo(char *accStr){
-     for (size_t i = 0; *(accStr + i) != '\0'; i++){
-        if (!(isdigit(*(accStr + i)) == 1)){
+void checkAccountNo(char *accStr)
+{
+    for (size_t i = 0; *(accStr + i) != '\0'; i++)
+    {
+        if (!(isdigit(*(accStr + i)) == 1))
             printf("Invalid Account Number!(Should be numbers ONLY)\n");
-            return -2;
-        }
     }
-
-    //atoi() changes any string to integer
-    unsigned long long int accInt = atoi(accStr);
-            printf("%s\n", accStr);
-    if (accInt > 9999999999 || accInt <= 0){
-        printf("Invalid Account Number!(shouldn't be more than 10-digits and greater than 0)\n");
-        return -1;
-    }
+    if (strlen(accStr) != 10)
+        printf("Invalid Account Number!(Should be 10-Digits)\n");
 
 }
 
-char *StrToUpper(char *str){
+char *StrToUpper(char *str)
+{
     for (size_t i = 0; str[i] != '\0'; i++)
         *(str + i) = toupper(*(str + i));
     return str;
--- final project.c
+++ final project.c
@@ -1,17 +1,49 @@
-// C Program to illustrate file opening
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 typedef struct
 {
-int mounth,year;
-}lastAccess;
+    int month,year;
+}Date;
+
 typedef struct
 {
-int account_number,mobile_number;
-char email_address[30];
-float balance;
-lastAccess dateOpened;
+    char mobile_number[12];
+    char account_number[11];
+    char username[20];
+    char email_address[30];
+    double balance;
+    Date dateOpened;
 
-}Amin;
+}Account;
+
+//Will be mainly used in ADD function
+Account *ConstructAccount(char *mobile, char *accNum, char *name, char *email, double balanceNum, Date *dateOpnd){
+    //Reserving memory in the heap for new instance
+    Account *acc = malloc(sizeof(Account));
+    if (acc == NULL){
+        printf("No enough space!\n");
+        return NULL;
+    }
+    //Copying the arguments into the new instance arguments
+    strcpy(acc->account_number, accNum);
+    strcpy(acc->mobile_number, mobile);
+    strcpy(acc->email_address, email);
+    strcpy(acc->username, name);
+    acc->balance = balanceNum;
+    acc->dateOpened.month = dateOpnd->month;
+    acc->dateOpened.year = dateOpnd->year;
+
+    return acc;
+}
+
+
+//Will be used before closing the program
+void DestructAccount(Account *acc){
+    free(acc);
+}
+
+int main() {
+
+}
