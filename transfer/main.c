be41f6d01dac45071a2c2e68a88133f0e28246e0
AmrZeina <142619031+AmrZeina@users.noreply.github.com>
2023-12-26 23:38:40 +0200

    Update transfer

--- transfer/main.c
+++ transfer/main.c
@@ -4,86 +4,97 @@ void Transfer(void)
 {
     char account_number1[11];
     char account_number2[11];
-    int amount;
-    char choice;
+    char amount1[sizeof(double)];
+    double amount2;
+    char choicee;
     char fileName1[16];
     char fileName2[16];
     int flag=0;
+    int flag2=0;
 
     do
     {
         do
         {
-            printf("%s","enter sender account number:");
+            printf("%s", "Enter Sender account number:");
             gets(account_number1);
-
         }
         while (checkAccountNo(account_number1)==0); //acc num invalid
+
         do
         {
-            printf("%s","enter receiver account number:");
+            printf("%s","Enter Receiver account number:");
             gets(account_number2);
+            if(strcmp(account_number1,account_number2)==0)
+            {
+              printf("Enter different reciever account");
+              flag2=1;
+            }
 
         }
-        while (checkAccountNo(account_number2)==0);  //acc num invalid
-        if(AccountExistenceCheck(account_number1)==0&&AccountExistenceCheck(account_number2)==0)//acc 1,2 are exist
+        while (checkAccountNo(account_number2) == 0 && flag2==1);  //acc num invalid
+
+        if(AccountExistenceCheck(account_number1) == 0 && AccountExistenceCheck(account_number2) == 0)//acc 1,2 are exist
         {
-            int index1=loadAccIndex(account_number1);
-            int index2=loadAccIndex(account_number2;
+            int index1 = loadAccIndex(account_number1);
+            int index2 = loadAccIndex(account_number2);
 
-             do
+            do
             {
-                printf("%s","enter an amount to transfer: ");
-                scanf("%d",&amount);
-                if(amount>accounts[index1].balance)
+                do
                 {
-                    printf("you only have %lf$ in your account",accounts[index1].balance);
-                    flag=1;
-                    break;
+                printf("%s", "Enter an amount to transfer: ");
+                gets(amount1);
 
+
+                }while(checkBalance(amount1)==0);
+                amount2=strtod(amount1,NULL);
+
+
+                if(amount2 > accounts[index1].balance)
+                {
+                    printf("You only have %lf$ in your account\n", accounts[index1].balance);
+                    flag = 1;
+                    break;
+                }
+            }
+            while(amount2 < 0);
+
+            if(flag == 0)
+            {
+                accounts[index1].balance -= amount2;
+                accounts[index2].balance += amount2;
+                sprintf(fileName1, "%s.txt", accounts[index1].account_number);
+                sprintf(fileName2, "%s.txt", accounts[index2].account_number);
+                accounts[index1].f_report = fopen(fileName1, "a");
+                if(accounts[index1].f_report == NULL)
+                {
+                    printf("Couldn't Find File!\n");
+                    exit(-1);
                 }
 
+                fprintf(accounts[index1].f_report, "You sent %lf$ to %s", amount2, account_number2);
+                fclose(accounts[index1].f_report);
+
+                accounts[index2].f_report = fopen(fileName2, "a");
+                if(accounts[index2].f_report == NULL)
+                {
+                    printf("Couldn't Find File!\n");
+                    exit(-1);
+                }
+                fprintf(accounts[index2].f_report, "You received %lf$ from %s", amount2, account_number2);
+                fclose(accounts[index2].f_report);
+
+                Save();
+                printf("Transaction Successful :)\n");
 
             }
-            while(amount <0);
-            if(flag==0)
-            {
-                    accounts[index1].balance-=amount;
-                    accounts[index2].balance+=amount;
-                    sprintf(fileName1,"%s.txt",accounts[index1].account_number);
-                    sprintf(fileName2,"%s.txt",accounts[index2].account_number);
-                    accounts[index1].f_report=fopen(fileName1,"a");
-                    if(accounts[index1].f_report==NULL)
-                    {
-                        printf("Couldn't find file!\n");
-                        exit(-1);
-                    }
-                    fprintf(accounts[index1].f_report,"you sent %d$ to %s",amount,account_number2);
-                    fclose(accounts[index1].f_report);
-
-                    accounts[index2].f_report=fopen(fileName2,"a");
-                    if(accounts[index2].f_report==NULL)
-                    {
-                        printf("Couldn't find file!\n");
-                        exit(-1);
-                    }
-                    fprintf(accounts[index2].f_report,"you received %d$ from %s",amount,account_number2);
-                    fclose(accounts[index2].f_report);
-                    save();
-                    printf("transaction successful");
-
-                    }
-                    else
-                    printf("transaction failed");
-
-
-             }
-
-        printf("Do you want to Withdraw again?(Y/N): ");
-        scanf("%c", &choice);
-
+            else
+                printf("Transaction Failed!\n");
+        }
+        printf("\nDo you want to Transfer again?(Y/N): ");
+        scanf("%c", &choicee);
+        getchar();
     }
-    while (choice=='y'||choice=='Y');
-
+    while (choicee == 'y' || choicee == 'Y');
 }
-
